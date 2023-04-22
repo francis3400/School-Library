98680804fee4dd95363ea4a9a6e42556ef446708
official-wisdom <wisdomowuamanam@outlook.com>
2023-04-22 13:52:26 +0100

    update student and teacher files

--- student.rb
+++ student.rb
@@ -1,9 +1,16 @@
-require './person'
+require_relative './person'
 
 class Student < Person
-  def initialize(age, classroom, _name = 'Unknown', _parent_permission: true)
-    super(age, _name = 'Unknown', _parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(age, classroom = 'Null', name = 'Unknown', parent_permission = 'true')
+    super(age, name, parent_permission)
     @classroom = classroom
+  end
+
+  def modify_classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
